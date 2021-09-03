981fc05af68657e04590838d720feab1b7af15de
windsovcreation <65227113+windsovcreation@users.noreply.github.com>
2021-09-04 01:11:24 +0300

    Added SquareTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -1,6 +1,8 @@
 package ru.stqa.pft.addressbook;
 
 import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -10,7 +12,7 @@ public class GroupCreationTests {
 
   @BeforeMethod(alwaysRun = true)
   public void setUp() throws Exception {
-    wd = new FirefoxDriver();
+    wd = new ChromeDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
   }
 
--- sandbox/src/main/java/ru/paket/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/paket/sandbox/MyFirstProgram.java
@@ -5,6 +5,9 @@ public class MyFirstProgram {
   public static void main(String[] args) {
     System.out.println("Hello, world!");
 
+    Square s = new Square(5);
+    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+
   }
 
   }
