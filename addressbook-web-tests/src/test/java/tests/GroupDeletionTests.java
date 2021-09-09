31197508ab4eb4e64b5d206d68b5950b1b5bddab
windsovcreation <65227113+windsovcreation@users.noreply.github.com>
2021-09-09 14:40:04 +0300

    Created 3 new packages

---
+++ addressbook-web-tests/src/test/java/tests/GroupDeletionTests.java
@@ -0,0 +1,16 @@
+package tests;
+
+import org.testng.annotations.*;
+
+public class GroupDeletionTests extends TestBase {
+
+  @Test
+  public void testGroupDeletion() throws Exception {
+    app.gotoGroupPage();
+    app.selectGroup();
+    app.deleteSelectedGroups();
+    app.returnToGroupPage();
+  }
+
+
+}
