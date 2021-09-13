e39d6adda29ca1cb69d8e1404beb01f060f4e25f
windsovcreation <65227113+windsovcreation@users.noreply.github.com>
2021-09-13 21:09:17 +0300

    Refactored Equation

--- sandbox/src/main/java/ru/paket/sandbox/Equation.java
+++ sandbox/src/main/java/ru/paket/sandbox/Equation.java
@@ -8,26 +8,35 @@ public class Equation {
 
     private int n;
 
-    public Equation (double a, double b, double c) {
+    public Equation(double a, double b, double c) {
 
         this.a = a;
         this.b = b;
         this.c = c;
 
-        double d = b*b - 4*a*c;
+        double d = b * b - 4 * a * c;
 
-        if (d > 0) {
-            n = 2;
+        if (a != 0) {
+            if (d > 0) {
+                n = 2;
+            } else if (d == 0) {
+                n = 1;
+            } else {
+                n = 0;
+            }
+
+        } else if (b != 0) {
+            n = 1;
+
+        } else if (c !=0) {
+            n = 0;
         } else {
-                if (d == 0) {
-                    n =1;
-                } else {
-                    n = 0;
-                }
+            n = -1;
         }
     }
 
-    public int rootNumber () {
-        return n;
+        public int rootNumber () {
+            return n;
+        }
     }
-}
+
--- sandbox/src/test/java/ru/paket/sandbox/EquationTests.java
+++ sandbox/src/test/java/ru/paket/sandbox/EquationTests.java
@@ -6,20 +6,34 @@ import org.testng.annotations.Test;
 public class EquationTests {
 
     @Test
-    public void test0(){
-    Equation e = new Equation (1, 1,1);
+    public void test0() {
+        Equation e = new Equation(1, 1, 1);
         Assert.assertEquals(e.rootNumber(), 0);
     }
 
     @Test
-    public void test1(){
-        Equation e = new Equation (1, 2,1);
+    public void test1() {
+        Equation e = new Equation(1, 2, 1);
         Assert.assertEquals(e.rootNumber(), 1);
     }
 
     @Test
-    public void test2(){
-        Equation e = new Equation (1, 5,6);
-        Assert.assertEquals(e.rootNumber(), 2);
+    public void testLinear() {
+        Equation e = new Equation(0, 1, 1);
+        Assert.assertEquals(e.rootNumber(), 1);
     }
+
+    @Test
+    public void testConstant() {
+        Equation e = new Equation(0, 0, 1);
+        Assert.assertEquals(e.rootNumber(), 0);
+    }
+
+    @Test
+    public void testZero() {
+        Equation e = new Equation(0, 0, 0);
+        Assert.assertEquals(e.rootNumber(), -1);
+    }
+
 }
+
