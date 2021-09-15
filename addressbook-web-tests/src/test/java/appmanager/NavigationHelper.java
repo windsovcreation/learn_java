ef761aa3795c4b655f2006e4e0a4dd940e712d79
windsovcreation <65227113+windsovcreation@users.noreply.github.com>
2021-09-15 19:52:11 +0300

    Optimized navigation helper

--- addressbook-web-tests/src/test/java/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/appmanager/NavigationHelper.java
@@ -10,13 +10,22 @@ public class NavigationHelper extends HelperBase {
     }
 
     public void gotoGroupPage() {
-      click (By.linkText("groups"));
+      if (isElementPresent(By.tagName("h1"))
+              && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+              && isElementPresent(By.name("new"))) {
+          return;
+      }
+        click (By.linkText("groups"));
     }
 
     public void gotoAddNewContactPage() {
         click (By.linkText("add new"));
     }
 
-    public void returnToHomePage() {click (By.linkText("home"));}
+    public void returnToHomePage() {
+        if (isElementPresent(By.id("maintable"))) {
+            return;
+        }
+        click (By.linkText("home"));}
 
 }
