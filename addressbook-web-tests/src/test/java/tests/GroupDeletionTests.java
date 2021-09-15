5bc7f1f27319aa4cba6ecfcec7057e5dd41bb646
windsovcreation <65227113+windsovcreation@users.noreply.github.com>
2021-09-15 20:21:13 +0300

    Added precondition check to create amd modify group tests

--- addressbook-web-tests/src/test/java/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/tests/GroupDeletionTests.java
@@ -1,5 +1,6 @@
 package tests;
 
+import model.GroupData;
 import org.testng.annotations.*;
 
 public class GroupDeletionTests extends TestBase {
@@ -7,6 +8,9 @@ public class GroupDeletionTests extends TestBase {
   @Test
   public void testGroupDeletion() {
     app.getNavigationHelper().gotoGroupPage();
+    if (! app.getGroupHelper().isThereAGroup()) {
+        app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returnToGroupPage();
