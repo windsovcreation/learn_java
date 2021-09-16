803ccee0e1e420155ef8903a3084796c8ef5082c
windsovcreation <65227113+windsovcreation@users.noreply.github.com>
2021-09-16 17:25:04 +0300

    Created Primes

---
+++ sandbox/src/test/java/ru/paket/sandbox/PrimeTests.java
@@ -0,0 +1,23 @@
+package ru.paket.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PrimeTests {
+
+    @Test
+    public void testPrime () {
+        Assert.assertTrue(Primes.isPrime(Integer.MAX_VALUE));
+    }
+
+    @Test( enabled = false)
+    public void testPrimeLong () {
+        long n = Integer.MAX_VALUE;
+        Assert.assertTrue(Primes.isPrime(n));
+    }
+
+    @Test
+    public void testNonPrime () {
+        Assert.assertTrue(Primes.isPrime(Integer.MAX_VALUE - 2));
+    }
+}
