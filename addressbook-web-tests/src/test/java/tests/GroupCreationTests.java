55fc7db06a4916c346dc9e3dc2f8dc014fd69484
windsovcreation <65227113+windsovcreation@users.noreply.github.com>
2021-09-21 17:12:35 +0300

    Added lambda to group creation tests

--- addressbook-web-tests/src/test/java/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/tests/GroupCreationTests.java
@@ -1,8 +1,8 @@
 package tests;
 
-import org.testng.Assert;
-import org.testng.annotations.*;
 import model.GroupData;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 import java.util.HashSet;
 import java.util.List;
@@ -20,13 +20,7 @@ public class GroupCreationTests extends TestBase {
     Assert.assertEquals(after.size(), before.size() + 1);
 
 
-    int max = 0;
-    for (GroupData g : after) {
-      if (g.getId() > max) {
-        max = g.getId();
-      }
-    }
-    group.setId(max);
+    group.setId(after.stream().max((o1,o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(group);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
 
