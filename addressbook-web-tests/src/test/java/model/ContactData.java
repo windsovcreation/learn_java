3e1392fbf2871a54cf1270026069b8ffee4e65ed
windsovcreation <65227113+windsovcreation@users.noreply.github.com>
2021-09-24 18:17:40 +0300

    Fixed fuckup

--- addressbook-web-tests/src/test/java/model/ContactData.java
+++ addressbook-web-tests/src/test/java/model/ContactData.java
@@ -1,6 +1,9 @@
 package model;
 
+import java.util.Objects;
+
 public class ContactData {
+    private final String id;
     private final String name;
     private final String surname;
     private final String address;
@@ -11,7 +14,21 @@ public class ContactData {
     private final String byear;
     private String group;
 
+    public ContactData(String id, String name, String surname, String address, String phone, String email, String bday, String bmonth, String byear, String group) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.address = address;
+        this.phone = phone;
+        this.email = email;
+        this.bday = bday;
+        this.bmonth = bmonth;
+        this.byear = byear;
+        this.group = group;
+    }
+
     public ContactData(String name, String surname, String address, String phone, String email, String bday, String bmonth, String byear, String group) {
+        this.id = null;
         this.name = name;
         this.surname = surname;
         this.address = address;
@@ -57,5 +74,31 @@ public class ContactData {
 
     public String getGroup() {
         return group;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "ContactData{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ContactData that = (ContactData) o;
+        return Objects.equals(id, that.id) && Objects.equals(name, that.name) && Objects.equals(surname, that.surname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, surname);
     }
 }
