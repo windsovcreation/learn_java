63447109a1ed0d5c7529c0c05e9ee5e1ae2865a0
windsovcreation <65227113+windsovcreation@users.noreply.github.com>
2021-09-25 13:47:42 +0300

    4.9

--- addressbook-web-tests/src/test/java/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/tests/ContactCreationTests.java
@@ -3,7 +3,6 @@ package tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import model.ContactData;
-
 import java.util.HashSet;
 import java.util.List;
 
@@ -20,16 +19,10 @@ public class ContactCreationTests extends TestBase {
     List<ContactData> after = app.getContactHelper().getContactList();
     Assert.assertEquals(after.size(), before.size() + 1);
 
-    int max =0;
-    for (ContactData c : after ) {
-      if (c.getId() > max) {
-        max = c.getId();
-      }
-    }
-    contact.setId(max);
+
+    contact.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(contact);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
-
-
+  
 }
