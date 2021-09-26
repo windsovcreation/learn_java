ec3d50a4280ec4890a1ba19f2b0493d75547187b
windsovcreation <65227113+windsovcreation@users.noreply.github.com>
2021-09-26 16:24:40 +0300

    5.2 for Group tests

--- addressbook-web-tests/src/test/java/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/tests/GroupModificationTests.java
@@ -2,32 +2,30 @@ package tests;
 
 import model.GroupData;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
 import java.util.Comparator;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Objects;
 
 public class GroupModificationTests extends TestBase {
 
-    @Test
-
-    public void testGroupModification () {
+    @BeforeMethod
+    public void ensurePreconditions () {
         app.getNavigationHelper().gotoGroupPage();
         if (! app.getGroupHelper().isThereAGroup()) {
             app.getGroupHelper().createGroup(new GroupData("test1", null, null));
         }
+    }
+
+    @Test
+    public void testGroupModification () {
         List<GroupData> before = app.getGroupHelper().getGroupList();
-        app.getGroupHelper().selectGroup(before.size() -1);
-        app.getGroupHelper().initGroupModification();
-        GroupData group = new GroupData(before.get(before.size() -1).getId(),"test1", "test2", "test3");
-        app.getGroupHelper().fillGroupForm(group);
-        app.getGroupHelper().submitGroupModification();
-        app.getGroupHelper().returnToGroupPage();
+        int index = before.size() -1;
+        GroupData group = new GroupData(before.get(index).getId(),"test1", "test2", "test3");
+        app.getGroupHelper().modifyGroup(index, group);
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size());
-        before.remove(before.size() - 1);
+        before.remove(index);
         before.add(group);
         Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
         before.sort(byId);
@@ -35,4 +33,6 @@ public class GroupModificationTests extends TestBase {
         Assert.assertEquals(before, after);
 
     }
+
+
 }
