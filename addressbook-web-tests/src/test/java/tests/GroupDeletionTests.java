d7259fa48d29323ff363888f148dbccd7b6cc812
windsovcreation <65227113+windsovcreation@users.noreply.github.com>
2021-09-27 18:36:33 +0300

    5.6 GD

--- addressbook-web-tests/src/test/java/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/tests/GroupDeletionTests.java
@@ -1,10 +1,14 @@
 package tests;
 
 import model.GroupData;
-import org.testng.Assert;
+import model.Groups;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import java.util.Set;
+
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.testng.Assert.assertEquals;
 
 public class GroupDeletionTests extends TestBase {
 
@@ -18,14 +22,12 @@ public class GroupDeletionTests extends TestBase {
 
   @Test
   public void testGroupDeletion() {
-    Set<GroupData> before = app.group().all();
+    Groups before = app.group().all();
     GroupData deletedGroup = before.iterator().next();
     app.group().delete(deletedGroup);
-    Set<GroupData> after = app.group().all();
-    Assert.assertEquals(after.size(), before.size() - 1);
-
-    before.remove(deletedGroup);
-    Assert.assertEquals(before, after);
+    Groups after = app.group().all();
+    assertEquals(after.size(), before.size() - 1);
+    assertThat(after, equalTo(before.without(deletedGroup)));
 
 
   }
